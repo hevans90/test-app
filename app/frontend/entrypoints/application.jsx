dc1789fb0eba9aeece50350a8cdce375c624b772
Harri Evans <hevans9000@gmail.com>
2024-07-04 11:10:20 +0100

    working react/vite/rails setup

---
+++ app/frontend/entrypoints/application.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return <h1>HELLO WORLD    eeeeeeeee</h1>;
+};
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
